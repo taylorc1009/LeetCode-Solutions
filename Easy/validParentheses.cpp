88bafbf861cf0ea1b9a0fa6ec1f6ea9eb33b6408
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-05-31 12:00:54 +0100

    Completed validParentheses

---
+++ Easy/validParentheses.cpp
@@ -0,0 +1,30 @@
+#include <stack>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+
+std::unordered_map<char, char> map = {
+    {'(', ')'},
+    {'{', '}'},
+    {'[', ']'}
+};
+
+bool isValid(std::string s) {
+    std::stack<char> parentheses;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (map.find(s[i]) != map.end())
+            parentheses.push(s[i]);
+        else if (map.at(parentheses.top()) == s[i])
+            parentheses.pop();
+        else
+            return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char** argv) {
+    std::string parentheses = "({[]}){}[()]";
+    std::cout << isValid(parentheses);
+}
