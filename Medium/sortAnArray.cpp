7c15199b31dd3852e047f61f8d2132c9e2d6eb6c
Taylor <taylorcourtney2000@googlemail.com>
2023-03-01 16:43:04 +0000

    sortAnArray: cleaned up code slightly

--- Medium/sortAnArray.cpp
+++ Medium/sortAnArray.cpp
@@ -5,14 +5,13 @@
 #include <algorithm>
 #include <string>
 
-std::vector<int> sortArray(std::vector<int>& nums) {// Counting Sort
+std::vector<int> sortArray(std::vector<int>& nums) { // Counting Sort
     const int nums_length = nums.size(),
         max = *max_element(nums.begin(), nums.end()),
         min = *min_element(nums.begin(), nums.end()),
         range = max - min + 1;
-
     std::vector<int> count(range), output(nums_length);
-    
+
     for (int i = 0; i < nums_length; i++)
         count[nums[i] - min]++;
 
@@ -28,10 +27,8 @@ std::vector<int> sortArray(std::vector<int>& nums) {// Counting Sort
 }
 
 int main(int argc, char** argv) {
-    std::vector<int> nums = {5, 1, 3, 0, 9, 2, 8, 7}, result = sortArray(nums);
-    std::string output = "";
+    std::vector<int> nums = {5, 1, 3, 0, 9, 9, 5, 2, 8, 7, 1}, result = sortArray(nums);
 
     for (std::vector<int>::const_iterator i = result.begin(); i != result.end(); i++)
-        output += std::to_string(*i) + (i < result.end() - 1 ? ", " : "");
-    std::cout << output;
+        std::cout << std::to_string(*i) + (i < result.end() - 1 ? " " : "");
 }
